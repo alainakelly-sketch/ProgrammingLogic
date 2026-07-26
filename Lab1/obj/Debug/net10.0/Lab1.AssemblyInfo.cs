35563fe13dc09c2cf196d27fb3c657241a6ad3be
alainakelly-sketch <alaina_kelly@icloud.com>
2026-07-26 22:02:25 +0000

    completed lab 4

--- Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5c01786c0b7b7d5e1a84cc8729d0612b93afc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2da49b08e1e317b739df001c5eef9e0b518474")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+
+class Car
+{
+    public string model;
+    public string color;
+    public int year;
+
+    // Example method to display car details.
+    public void Display()
+    {
+        Console.WriteLine("Car Details:");
+        Console.WriteLine("Model: " + model);
+        Console.WriteLine("Color: " + color);
+        Console.WriteLine("Year: " + year);
+    }
+    //Problem 1
+     public void Start()
+    {
+        Console.WriteLine("The car is starting.");
+    }
+    //Problem 2
+    public void Drive(int miles)
+    {
+        Console.WriteLine("The car drove " + miles + " miles.");
+    }
+    //Problem 3
+    public string GetDescription()
+    {
+        return year + " " + color + " " + model;
+    }
+    //Problem 4
+    public void Repaint(string newColor)
+    {
+        color = newColor;
+        Console.WriteLine("The car has been repainted " + newColor + ".");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating a Car object and assigning values to its properties
+        Car myCar = new Car();
+        myCar.model = "Civic";
+        myCar.color = "Black";
+        myCar.year = 2020;
+
+        myCar.Display(); // Calling the Display method
+        myCar.Start(); //Problem 1
+        myCar.Drive(5); //Problem 2
+        //Problem 3
+        string description = myCar.GetDescription();
+        Console.WriteLine(description);
+        //Problem 4
+        myCar.Repaint("Red");
+        myCar.Display();
+    }
+}
+
+//Problem 1
+
